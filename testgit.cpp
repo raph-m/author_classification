9caaef74b5232a712a80bc02de926350cee336fc
Redouane <redouanedziri@gmail.com>
2017-10-26 17:37:29 +0200

    le premier commit de Redouane Dziri

--- testgit.cpp
+++ testgit.cpp
@@ -11,5 +11,6 @@ void test() {
     cout << "My name is Raphael Montaud2" << endl;
     cout << "My name is Raphael Montaud3" << endl;
     cout << "My name is Raphael Montaud4" << endl;
+	cout << "My name is Redouane Dziri" << endl;
 }
 
