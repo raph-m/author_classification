815fc9760e3a06d7a1aa35c06df75d40dcac17af
Vardoom <paul.vardon@polytechnique.edu>
2017-11-07 14:33:05 +0100

    le premier commit de Paul Vardon

--- testgit.cpp
+++ testgit.cpp
@@ -11,7 +11,8 @@ void test() {
     cout << "My name is Raphael Montaud2" << endl;
     cout << "My name is Raphael Montaud3" << endl;
     cout << "My name is Raphael Montaud4" << endl;
-	cout << "My name is Redouane Dziri" << endl;
+    cout << "My name is Redouane Dziri" << endl;
     cout << "My name is Arnaud Stiegler" << endl;
+    cout << "My name is Paul Vardon" << endl;
 }
 
