de1db8a5923086d762e0f8182bfbd6121fed5aed
Luca <luca.ganassali@polytechnique.edu>
2017-11-14 17:43:47 +0100

    Luca pour sentenceCount

--- block.cpp
+++ block.cpp
@@ -1,5 +1,6 @@
 #include "block.h"
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -13,13 +14,6 @@ Block::Block(long number, vector<char> text){
     this->textUnit = text;
 }
 
-Block::Block(long number, vector<char> text, vector<int> f1, int f2, int f3){
-    this->id = number;
-    this->textUnit = text;
-    this->charCount = f1;
-    this->wordCount = f2;
-    this->sentenceCount = f3;
-}
 
 void Block::updateWordCount(){
     int cmpt = 1;
@@ -31,17 +25,32 @@ void Block::updateWordCount(){
     this->wordCount = cmpt;
 }
 
-void Block::updateSentenceCount(){
-    int cmpt = 1;
-    for (vector<char>::iterator it = this->textUnit.begin(); it != this->textUnit.end(); ++it) {
-        if (*it == '.' || *it == '!' || *it == '?') {
-            cmpt++;
+void Block::updateSentenceCount(){//sentenceCount is set to the number of sentences, separators are '.', '!', '?', or '-'
+    sentenceCount = 0;
+    vector<char> separators ; //vector with all separators
+    separators.push_back('.');
+    separators.push_back('!');
+    separators.push_back('?');
+    separators.push_back('-');
+
+    int l = textUnit.size(); //always 1000, but we never know what may happen :)
+    int mem = 0; //turns to 1 if a separator is met
+
+
+    for (int i = 0; i<l; i++){
+        if (find(separators.begin(), separators.end(), textUnit[i]) != separators.end()){//we check whether the char is a separator
+            if (!mem){ //if the previous character was not a separator, it means that we reached the end of a sentence
+                sentenceCount+=1;
+                mem=1;
+            }
+        }
+        else{
+            mem=0;
         }
     }
-    this->sentenceCount = cmpt;
 }
 
-void Block::updateCharCount(){// takes into account characters from 32 (space) to 126 (~),charCount is set to an int vector of size 95
+void Block::updateCharFrequency(){// takes into account characters from 32 (space) to 126 (~),charFrequency is set to an int vector of size 95
     int l = textUnit.size(); //always 1000, but we never know what may happen :)
     int ascii;
     vector<int> res(95,0);
@@ -51,6 +60,6 @@ void Block::updateCharCount(){// takes into account characters from 32 (space) t
             res[ascii-32]+=1; //we add 1 to the corresponding indicator in the vector
         }
     }
-    charCount = res;
+    charFrequency = res;
 }
 
