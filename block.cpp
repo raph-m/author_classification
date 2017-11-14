c576760a63a40b768925bda8392b2e70677e9e00
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2017-11-14 16:57:37 +0100

    fix previous commit

--- block.cpp
+++ block.cpp
@@ -1,15 +1,19 @@
 #include "block.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 Block::Block()
 {
 }
 
-Block::Block(long number, std::vector<char> text){
+Block::Block(long number, vector<char> text){
     this->id = number;
     this->textUnit = text;
 }
 
-Block::Block(long number, std::vector<char> text, int* f1, int f2, int f3){
+Block::Block(long number, vector<char> text, vector<int> f1, int f2, int f3){
     this->id = number;
     this->textUnit = text;
     this->charCount = f1;
@@ -19,7 +23,7 @@ Block::Block(long number, std::vector<char> text, int* f1, int f2, int f3){
 
 void Block::updateWordCount(){
     int cmpt = 1;
-    for (std::vector<char>::iterator it = this->textUnit.begin(); it != this->textUnit.end(); ++it) {
+    for (vector<char>::iterator it = this->textUnit.begin(); it != this->textUnit.end(); ++it) {
         if (*it == ' ' || *it == '/') {
             cmpt++;
         }
@@ -29,10 +33,24 @@ void Block::updateWordCount(){
 
 void Block::updateSentenceCount(){
     int cmpt = 1;
-    for (std::vector<char>::iterator it = this->textUnit.begin(); it != this->textUnit.end(); ++it) {
+    for (vector<char>::iterator it = this->textUnit.begin(); it != this->textUnit.end(); ++it) {
         if (*it == '.' || *it == '!' || *it == '?') {
             cmpt++;
         }
     }
     this->sentenceCount = cmpt;
 }
+
+void Block::updateCharCount(){// takes into account characters from 32 (space) to 126 (~),charCount is set to an int vector of size 95
+    int l = textUnit.size(); //always 1000, but we never know what may happen :)
+    int ascii;
+    vector<int> res(95,0);
+    for (int i = 0; i<l; i++){
+        ascii = (int) textUnit[i];
+        if ((ascii>=32)&(ascii<=126)){
+            res[ascii-32]+=1; //we add 1 to the corresponding indicator in the vector
+        }
+    }
+    charCount = res;
+}
+
--- block.h
+++ block.h
@@ -1,24 +1,29 @@
 #ifndef BLOCK_H
 #define BLOCK_H
+#include <iostream>
 #include <vector>
+
+using namespace std;
 
 class Block
 {
 public:
 
     long id;
-    std::vector<char> textUnit;
+    vector<char> textUnit;
 
-    int* charCount;
+    vector<int> charCount;
     int wordCount;
     int sentenceCount;
 
     Block();
-    Block(long number, std::vector<char> text);
-    Block(long number, std::vector<char> text,int* f1,int f2,int f3);
+    Block(long number, vector<char> text);
+    Block(long number, vector<char> text,vector<int> f1,int f2,int f3);
 
     void updateWordCount();
     void updateSentenceCount();
+    void updateCharCount();
+
 };
 
 #endif // BLOCK_H
