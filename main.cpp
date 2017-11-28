431845e5c0e0c9c2952388120e829c45e28b17ee
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2017-11-28 14:35:57 +0100

    Fixin file reader

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <Preprocessing/text.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    std::cout<< "Hello world ! " << std::endl;
-    return a.exec();
+
+    text test0 ;
+    test0.id = 27;
+    std::cout<< test0.id << std::endl;
+    std::cout<< test0.author << std::endl;
+    std::cout<< test0.title << std::endl;
+    test0.createBlockList();
+
+
+
 }
 
