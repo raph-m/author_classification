e073dd25aa85346fe01c698daa1e5e046318edef
Vardoom <paul.vardon@polytechnique.edu>
2017-11-28 15:29:32 +0100

    main

--- main.cpp
+++ main.cpp
@@ -8,13 +8,6 @@ int main(int argc, char *argv[])
     text test0 ;
     test0.id = 27;
     std::cout<< test0.id << std::endl;
-    std::cout<< test0.author << std::endl;
-    std::cout<< test0.title << std::endl;
     test0.createBlockList();
-    std::vector<std::map<std::string, std::string> > database;
-
-
-
 
 }
-
