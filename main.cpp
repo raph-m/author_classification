8d7dc509f2c2877181ccd9cccc6948f8eca13627
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2017-11-28 15:35:14 +0100

    erratum

--- main.cpp
+++ main.cpp
@@ -10,5 +10,11 @@ int main(int argc, char *argv[])
     std::cout<< test0.id << std::endl;
     test0.createBlockList();
 
+    test0.blockList[0].updateDifferentWordsCount();
+    test0.blockList[0].updateUnicWordsCount();
+
+    std::cout<< std::to_string(test0.blockList[0].differentWordsCount) << std::endl;
+    std::cout<< std::to_string(test0.blockList[0].unicWordsCount) << std::endl;
+
 }
 
