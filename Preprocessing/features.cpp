c9a6119eca96d3b2052b2a798c7ac7ec5dbe0f1a
basile73 <basile.confavreux@polytechnique.edu>
2017-12-05 15:58:57 +0100

    ajout d'un fichier id.txt en sortie

--- Preprocessing/features.cpp
+++ Preprocessing/features.cpp
@@ -24,8 +24,10 @@ Features::Features(long number, std::vector<int> wordF,int wordC,std::vector<int
 void Features::createcsv(){ // A UPDATER A CHAQUE FOIS QU ON AJOUTE UN FEATURE //
     string name=text::intToString(id);
     std::ofstream myfile("../results.txt", std::ios::in);
+     std::ofstream myfile2("../id.txt", std::ios::in);
     myfile.seekp(0, ios::end); // On se déplace à la fin du fichier
-    myfile << name+",";
+    myfile2.seekp(0, ios::end); // On se déplace à la fin du fichier
+    myfile2 << name+"\n";
     myfile << text::floatToString(this->wordCount)+",";
     for (int i=0; i<charFrequency.size();i++){
         myfile << text::floatToString(this->charFrequency.at(i))+",";
@@ -39,5 +41,5 @@ void Features::createcsv(){ // A UPDATER A CHAQUE FOIS QU ON AJOUTE UN FEATURE /
     myfile << text::floatToString(this->differentWordsCount);
     myfile << "\n";
     myfile.close();
-}
-;
+    myfile2.close();
+};
