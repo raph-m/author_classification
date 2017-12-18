c9a81dc652c37c6321f3de761bd96984b3ae341e
raph-m <raphael.montaud@riminder.net>
2017-12-18 12:13:08 +0100

    changed input files

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@
 int main(int argc, char *argv[]){
     int nbriteration = 6;
     std::map<int, std::string> idToAuthor;
-    //idToAuthor = createTextFeatures(nbriteration);
-    //createtxtFile(nbriteration, "results"); //pour entrainer sur le dataset gutemberg. Ne marche que si le dataset+ fichier vide "results.txt" vide à la racine du projet
+    idToAuthor = createTextFeatures(nbriteration);
     int lineCount = 0; //nbre de ligne du fichier csv qui résultera du texte test
-    lineCount=preprocessingTest("../27.txt","txt", "testResult"); //pour tester un fichier .txt (à la racine du projet) dont l'auteur est à déterminer
+    lineCount=preprocessingTest("../1013.txt", "txt", "testResult");
     return 0;
 }
